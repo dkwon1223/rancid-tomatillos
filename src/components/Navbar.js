8a1e5da01d36f4fed22401f0adf080fb90d418ad
Erin Kelley <erin.kelley01@gmail.com>
2024-03-23 22:28:13 -0600

    Add border radius to movie cards

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,8 +7,12 @@ export default function Navbar({ search, setSearch }) {
             <div className='nav-logo'> 
                 <img src={TomatilloIcon}/>
                 <h1>Rancid Tomatillos</h1>
-                <input type="search" placeholder='Search by Movie Title' value={search} onChange={(event) => setSearch(event.target.value)}/>
             </div>
+                <div className='search-bar'>
+                  <input className='search-input' type="search" placeholder='Search by Movie Title' value={search} onChange={(event) => setSearch(event.target.value)}/>  
+                </div>
+                
+         
         </nav>
     )
 }
