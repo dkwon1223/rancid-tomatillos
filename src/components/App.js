85ca40d77cf3048a5ea444f204e2a259b856981e
David Kwon <112133897+dkwon1223@users.noreply.github.com>
2024-03-23 18:49:34 -0600

    Refactor all movie and single movie state to use fetch data from API

--- src/components/App.js
+++ src/components/App.js
@@ -6,18 +6,50 @@ import movieData from '../movieData';
 import { useState } from "react";
 
 export default function App() {
-  const [movies, setMovies] = useState(movieData.movies)
+  const [movies, setMovies] = useState([])
   const [movie, setMovie] = useState([])
   const [details, setDetails] = useState(false);
  
-  function returnToHome(){
-    setDetails(false)
+  async function getAllMovies() {
+    await fetch("https://rancid-tomatillos.herokuapp.com/api/v2/movies")
+            .then(response => {
+              if(!response.ok) {
+                throw new Error("Unable to retrieve movies. Try again later") 
+              }
+              return response.json()
+            })
+            .then(data => {
+              setMovies(data.movies);
+            })
+            .catch(error => {
+              console.log(error);
+            })
   }
 
-  function getMovieDetails(id){
-    let singleMovie = movies.find(movie => movie.id === id)
-    setDetails(true)
-    setMovie(singleMovie)
+  getAllMovies();
+
+  function returnToHome(){
+    setDetails(false);
+  }
+
+  async function getMovieDetails(id){
+    // let singleMovie = movies.find(movie => movie.id === id)
+    // setDetails(true)
+    // setMovie(singleMovie)
+    await fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
+            .then(response => {
+              if(!response.ok) {
+                throw new Error(`Unable to movie ID(${id}). Try again later`) 
+              }
+              return response.json();
+            })
+            .then(data => {
+              setDetails(true);
+              setMovie(data.movie);
+            })
+            .catch(error => {
+              console.log(error);
+            })
   }
 
   return (
--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -3,9 +3,19 @@ import '../styles/MovieDetails.scss';
 function MovieDetails({ returnToHome, movie }){
     return (
         <section className="detail-container">
-            <h1>{movie.title}</h1>
-            <img src={movie.backdrop_path}/>
-            <button onClick={returnToHome}>Back to Homepage</button> 
+            <article>
+                <h1>{movie.title}</h1>
+                <h2>Audience Rating: {movie.average_rating}</h2>
+                <h2>Released: {movie.release_date}</h2>
+                <h3>Budget: ${Intl.NumberFormat().format(movie.budget)}</h3>
+                <h3>Revenue: ${Intl.NumberFormat().format(movie.revenue)}</h3>
+                <h3>Runtime: {movie.runtime}</h3>
+                <h2>{movie.tagline}</h2>
+                <p>{movie.overview}</p>
+                <p>{movie.genres.join(" - ")}</p>
+                <button onClick={returnToHome}>Back to Homepage</button> 
+            </article>
+            <img src={movie.backdrop_path} className='detailed-backdrop-image'/>
         </section>
     )
 }
