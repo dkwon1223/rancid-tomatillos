7609e9128805deeed3145831884d3d8f752ecbb7
David Kwon <dkwon1223@gmail.com>
2024-03-26 22:48:34 -0600

    Add return home link in NotFound component

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import '../styles/Error.scss';
-
+import { Link } from 'react-router-dom';
 export default function NotFound() {
   return (
     <div className='error-container'>
         <h1>Page not found!</h1>
+        <Link to={"/"}>Return Home</Link>
     </div>
   )
 }
