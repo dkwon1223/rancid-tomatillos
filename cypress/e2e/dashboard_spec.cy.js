90e92a4cae547aaa72f8a19024867db53b9310e3
Erin Kelley <erin.kelley01@gmail.com>
2024-03-28 15:09:19 -0600

    Add fixture for movie details

--- cypress/e2e/dashboard_spec.cy.js
+++ cypress/e2e/dashboard_spec.cy.js
@@ -4,24 +4,11 @@ describe('Loading the homepage', () => {
     {
       statusCode: 200, 
       fixture: "movies" 
-      
-    //   { movies: [{
-    //     id: 436270,
-    //     poster_path: "https://image.tmdb.org/t/p/original//pFlaoHTZeyNkG83vxsAJiGzfSsa.jpg",
-    //     backdrop_path: "https://image.tmdb.org/t/p/original//bQXAqRx2Fgc46uCVWgoPz5L5Dtr.jpg",
-    //     title: "Black Adam",
-    //     average_rating: 4,
-    //     release_date: "2022-10-19"
-    //   },
-    //   {
-    //     id: 436271,
-    //     poster_path: "https://image.tmdb.org/t/p/original//pFlaoHTZeyNkG83vxsAJiGzfSsa.jpg",
-    //     backdrop_path: "https://image.tmdb.org/t/p/original//bQXAqRx2Fgc46uCVWgoPz5L5Dtr.jpg",
-    //     title: "Cool Adam",
-    //     average_rating: 5,
-    //     release_date: "2022-10-19"
-    //   }
-    // ]}
+    })
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/436270',
+    {
+      statusCode: 200, 
+      fixture: "movieDetails" 
     })
     cy.visit('http://localhost:3000')
   })
@@ -36,11 +23,11 @@ describe('Loading the homepage', () => {
     .get('div').contains('h3', '4')
     .get('.movie-card').last().contains('h2', 'R.I.P.D. 2: Rise of the Damned')
     .get('.movie-card').contains("Black").click()
-    .url().should('eq', 'http://localhost:3000/')
+    .url().should('eq', 'http://localhost:3000/movies/436270')
   })
 
   it('Should filter movies based on search input', () => {
-    cy.get('input[type=search').type('The Woman')
+    cy.get('input[type=search').type('The Woman').should('have.value', 'The Woman')
     .get('.movie-container').contains('h2', 'The Woman King')
     .get('.movie-container').contains('h2', '2022')
     .get('.movie-container').contains('h3', '7')
