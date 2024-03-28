1687af3494ac483ab06572a195c479e9ed58b840
Erin Kelley <erin.kelley01@gmail.com>
2024-03-28 14:38:42 -0600

    Add fixture

--- cypress/e2e/dashboard_spec.cy.js
+++ cypress/e2e/dashboard_spec.cy.js
@@ -3,23 +3,25 @@ describe('Loading the homepage', () => {
     cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies',
     {
       statusCode: 200, 
-      body: { movies: [{
-        id: 436270,
-        poster_path: "https://image.tmdb.org/t/p/original//pFlaoHTZeyNkG83vxsAJiGzfSsa.jpg",
-        backdrop_path: "https://image.tmdb.org/t/p/original//bQXAqRx2Fgc46uCVWgoPz5L5Dtr.jpg",
-        title: "Black Adam",
-        average_rating: 4,
-        release_date: "2022-10-19"
-      },
-      {
-        id: 436271,
-        poster_path: "https://image.tmdb.org/t/p/original//pFlaoHTZeyNkG83vxsAJiGzfSsa.jpg",
-        backdrop_path: "https://image.tmdb.org/t/p/original//bQXAqRx2Fgc46uCVWgoPz5L5Dtr.jpg",
-        title: "Cool Adam",
-        average_rating: 5,
-        release_date: "2022-10-19"
-      }
-    ]}
+      fixture: "movies" 
+      
+    //   { movies: [{
+    //     id: 436270,
+    //     poster_path: "https://image.tmdb.org/t/p/original//pFlaoHTZeyNkG83vxsAJiGzfSsa.jpg",
+    //     backdrop_path: "https://image.tmdb.org/t/p/original//bQXAqRx2Fgc46uCVWgoPz5L5Dtr.jpg",
+    //     title: "Black Adam",
+    //     average_rating: 4,
+    //     release_date: "2022-10-19"
+    //   },
+    //   {
+    //     id: 436271,
+    //     poster_path: "https://image.tmdb.org/t/p/original//pFlaoHTZeyNkG83vxsAJiGzfSsa.jpg",
+    //     backdrop_path: "https://image.tmdb.org/t/p/original//bQXAqRx2Fgc46uCVWgoPz5L5Dtr.jpg",
+    //     title: "Cool Adam",
+    //     average_rating: 5,
+    //     release_date: "2022-10-19"
+    //   }
+    // ]}
     })
     cy.visit('http://localhost:3000')
   })
@@ -29,18 +31,19 @@ describe('Loading the homepage', () => {
   })
   
   it('Should show a grid display of different movies', () => {
-    cy.get('div').contains('h2', 'Black Adam')
+    cy.get('div').first().contains('h2', 'Black Adam')
     .get('div').contains('h2', '2022')
-    .get('div').contains('h3', '5')
+    .get('div').contains('h3', '4')
+    .get('.movie-card').last().contains('h2', 'R.I.P.D. 2: Rise of the Damned')
     .get('.movie-card').contains("Black").click()
     .url().should('eq', 'http://localhost:3000/')
   })
 
   it('Should filter movies based on search input', () => {
-    cy.get('input[type=search').type('Cool')
-    .get('.movie-container').contains('h2', 'Cool Adam')
+    cy.get('input[type=search').type('The Woman')
+    .get('.movie-container').contains('h2', 'The Woman King')
     .get('.movie-container').contains('h2', '2022')
-    .get('.movie-container').contains('h3', '5')
+    .get('.movie-container').contains('h3', '7')
     .get('h2').should('not.contain', 'Black Adam')
   })
   }
